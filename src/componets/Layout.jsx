3b851bbebde6bec6f9a87bc1810165a066ccdbaa
AlexLL8416 <114562279+AlexLL8416@users.noreply.github.com>
2025-10-07 21:06:02 +0200

    Layout y fondo de gestor alimentos

---
+++ src/componets/Layout.jsx
@@ -0,0 +1,22 @@
+import "./Layout.css"
+
+function LayoutInicio(){
+    return(
+        <div className="layout">
+            <div className="enlace">
+                <p>Home</p>
+            </div>
+            <div className="enlace">
+                <p>Estudios</p>
+            </div>
+            <div className="enlace">
+                <p>Proyectos</p>
+            </div>
+            <div className="enlace">
+                <p>Contacto</p>
+            </div>
+        </div>
+    )
+}
+
+export default LayoutInicio
