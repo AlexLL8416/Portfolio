547c8de6ae1175e646d82e33f4917dcdc900b048
AlexLL8416 <114562279+AlexLL8416@users.noreply.github.com>
2025-10-15 18:58:35 +0200

    Página Hand Mouse Empezada

--- src/pages/Hand_Mouse.jsx
+++ src/pages/Hand_Mouse.jsx
@@ -1,12 +1,15 @@
 import "./Hand_Mouse.css"
 import FaultyTerminal from "../componets/FaultyTerminal"
+import OverlayPages from "../componets/OverlayPages"
+import TextType from "../componets/TextTipe"
+{/*import video from "../assets/video.mp4"*/}
 
 function PaginaHand_Mouse(){
     return(
-        <div>
+        <div className="pagina-hand-mouse">
             <div className="fondo">
                 <FaultyTerminal
-                    scale={1.5}
+                    scale={1}
                     gridMul={[2, 1]}
                     digitSize={1.2}
                     timeScale={0.1}
@@ -21,12 +24,61 @@ function PaginaHand_Mouse(){
                     tint="#A7EF9C"
                     mouseReact={true}
                     mouseStrength={0.05}
-                    pageLoadAnimation={true}
-                    brightness={1}
+                    pageLoadAnimation={false}
+                    brightness={0.4}
                 />
             </div>
+            <OverlayPages/>
+
             <div className="contenido">
-                <h1>Página del Hand Mouse</h1>
+                <div id="proyecto">
+                <Inicio/>
+                </div>
+                <div className="separador"></div>
+                <div id="codigo">
+                {/*<Codigo />*/}
+                </div>
+                <div className="separador"></div>
+                <div id="contacto">
+                {/*<Contacto />*/}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Inicio(){
+    return(
+        <div className="inicioContainer">
+            <TextType
+                text={["Hand Mouse","OpenCV", "MediaPipe"]}
+                typingSpeed={75}
+                pauseDuration={3000}
+                className='tituloInicio'
+            />
+            <div className="inicio">
+                <div className="inicioIzquierda">
+                    <video autoPlay muted playsInline loop controls={false} className="video">
+                        {/*<source src={video} type="video/mp4"/>*/}
+                        Tu navegador no soporta el video.
+                    </video>
+                    <p className="enlaceRepo" onClick={()=>{window.open("https://github.com/AlexLL8416/Hand_Mouse","blank_")}}>Enlace del proyecto</p>
+                </div>
+                <div className="textoInicio">
+                    <p>
+                        <strong>Hand Mouse</strong> es un proyecto de visión por computador que permite controlar el cursor del ratón mediante 
+                        <strong> gestos de la mano detectados a través de la cámara web</strong>.
+                    </p>
+                    <p>
+                        Desarrollado con <strong>Python</strong>, <strong>OpenCV</strong> y <strong>MediaPipe</strong>, traduce en tiempo real los movimientos de la mano 
+                        en acciones del cursor, como moverlo, hacer clic o cerrar el programa, sin necesidad de tocar el ratón físico.
+                    </p>
+                    <p>
+                        Diseñado como una demostración de <strong>interfaces basadas en gestos</strong> y 
+                        <strong>aplicaciones prácticas de la visión artificial</strong>, este proyecto combina tecnología, accesibilidad e 
+                        interacción natural entre el usuario y el ordenador.
+                    </p>
+                </div>
             </div>
         </div>
     )
